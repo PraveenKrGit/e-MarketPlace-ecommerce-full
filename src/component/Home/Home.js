c0b7b8262c2bb21afc7e93ecd10ac4dcca0ff591
Praveen Kumar Yadav <30688435+PraveenKrGit@users.noreply.github.com>
2023-09-03 10:34:24 +0530

    json changes

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,5 +1,4 @@
 import React, { Fragment, useEffect } from "react";
-import { CgMouse } from "react-icons/all";
 import "./Home.css";
 import ProductCard from "./ProductCard.js";
 import MetaData from "../layout/MetaData";
